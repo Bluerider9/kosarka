936e2ef024ae7728571d67a0b2fe591559da674e
Bluerider9 <54979069+Bluerider9@users.noreply.github.com>
2025-06-24 00:17:41 +0200

    app.py

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import streamlit as st
 import fitz  # PyMuPDF
 from io import BytesIO
@@ -12,7 +11,7 @@ kategorija = st.selectbox(
     "Kategorija",
     [" ", "Škola košarke", "U12", "U14", "U16", "SEN"]
 )
-broj_treninga = st.number_input("Broj treninga", step=0, min_value=1)
+broj_treninga = st.number_input("Broj treninga", step=1, min_value=1)
 broj_igraca = st.number_input("Broj igrača na treningu", step=1, min_value=1)
 uvod = st.text_area("Uvod")
 glavni_dio = st.text_area("Glavni dio")
@@ -20,23 +19,22 @@ zavrsni_dio = st.text_area("Završni dio")
 
 if st.button("Generiraj PDF"):
     template_path = "spranca.pdf"
-    font_path = "Roboto.ttf"  # mora biti u istom direktoriju
 
     doc = fitz.open(template_path)
     page = doc[0]
 
-    font = fitz.Font(fontfile=font_path)
     fs_title = 18
     fs_text = 14
+    fontname = "helv"  # možeš koristiti 'helv', 'times', 'courier', itd.
 
     # Dodavanje teksta na okvirne pozicije (prilagodljivo)
-    page.insert_text((110, 62), f"{datum}", fontsize=14, font=font)
-    page.insert_text((110, 30), f"{kategorija}", fontsize=fs_title, font=font)
-    page.insert_text((552, 30), f"{broj_treninga}", fontsize=fs_text, font=font)
-    page.insert_text((552, 62), f"{broj_igraca}", fontsize=fs_text, font=font)
-    page.insert_text((230, 165), uvod, fontsize=fs_text, font=font)
-    page.insert_text((230, 325), glavni_dio, fontsize=fs_text, font=font)
-    page.insert_text((230, 570), zavrsni_dio, fontsize=fs_text, font=font)
+    page.insert_text((110, 62), f"{datum}", fontsize=fs_text, fontname=fontname)
+    page.insert_text((110, 30), f"{kategorija}", fontsize=fs_title, fontname=fontname)
+    page.insert_text((552, 30), f"{broj_treninga}", fontsize=fs_text, fontname=fontname)
+    page.insert_text((552, 62), f"{broj_igraca}", fontsize=fs_text, fontname=fontname)
+    page.insert_text((230, 165), uvod, fontsize=fs_text, fontname=fontname)
+    page.insert_text((230, 325), glavni_dio, fontsize=fs_text, fontname=fontname)
+    page.insert_text((230, 570), zavrsni_dio, fontsize=fs_text, fontname=fontname)
 
     # Spremanje u memoriju
     output_pdf = BytesIO()
